324f36bc024802ddf80b8f4ccbc63bb916b75167
Morgan <morgan@wilde.mx>
2020-10-09 18:24:51 +1100

    Created ACF, menu items, and templates for posts

--- wp-content/themes/canberra/lib/enqueue-scripts.php
+++ wp-content/themes/canberra/lib/enqueue-scripts.php
@@ -16,3 +16,42 @@ function admin_style() {
   wp_enqueue_style('admin-styles', get_template_directory_uri() . '/admin.css');
 }
 add_action('admin_enqueue_scripts', 'admin_style');
+
+// custom post type function
+add_action('init', 'create_custom_post_types');
+function create_custom_post_types() {
+  register_post_type('article', array(
+    'labels' => array(
+      'name' => 'News',
+      'singular_name' => 'Article',
+      'add_new_item' => 'Add New Article'
+    ),
+    'public' => true,
+    'has_archive' => true,
+    'menu_icon' => 'dashicons-welcome-widgets-menus',
+    'rewrite' => array('slug' => 'article'),
+    'supports' => array('title', 'editor', 'thumbnail')
+  ));
+  register_post_type('house', array(
+    'labels' => array(
+      'name' => 'Houses',
+      'singular_name' => 'House',
+      'add_new_item' => 'Add New House'
+    ),
+    'public' => true,
+    'has_archive' => true,
+    'menu_icon' => 'dashicons-admin-home',
+    'rewrite' => array('slug' => 'house'),
+  ));
+  register_post_type('architect', array(
+    'labels' => array(
+      'name' => 'Architects',
+      'singular_name' => 'Architect',
+      'add_new_item' => 'Add New Architect'
+    ),
+    'public' => true,
+    'has_archive' => true,
+    'menu_icon' => 'dashicons-businessperson',
+    'rewrite' => array('slug' => 'architect'),
+  ));
+}
---
+++ wp-content/themes/canberra/template-article.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ *  Template Name: Article
+ */
+
+get_header();
+
+$page_components = get_field('article_components');
+if ($page_components) {
+  foreach ($page_components as $page_component) {
+    $component_name = str_replace('_', '-', $page_component['acf_fc_layout']);
+    global $fields;
+    $fields = $page_component;
+    get_template_part("components/$component_name/$component_name");
+  }
+}
+
+get_footer();
