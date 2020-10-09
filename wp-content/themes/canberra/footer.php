25461d77d7d827e0fb4ae89b243087309bf877f2
Morgan <morgan@wilde.mx>
2020-10-09 16:20:00 +1100

    Created the footer and fixed the header

--- wp-content/themes/canberra/footer.php
+++ wp-content/themes/canberra/footer.php
@@ -1,3 +1,23 @@
+<footer>
+  <div class="container">
+    <div class="grid-col-3">
+      <div class="logo">
+        <img src="<?= get_img_url('logo.png') ?>">
+      </div>
+      <div class="menu-col">
+        <h6>Company</h6>
+        <?php wp_nav_menu(array('echo' => true)); ?>
+      </div>
+      <div class="small-text">
+        <p>By Morgan Wilde, Stef, Millie</p>
+        <p>Site for Backend Web Development [11058]</p>
+      </div>
+    </div>
+  </div>
+
+</footer>
+
+
 <?php wp_footer(); ?>
 
 </body>
