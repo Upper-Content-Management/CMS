335f4febc699b2ea749f4e5b698b817bb2c3a15e
Morgan <morgan@wilde.mx>
2020-10-10 12:32:52 +1100

    Created gallery for house

--- wp-content/themes/canberra/components/house/gallery/gallery.php
+++ wp-content/themes/canberra/components/house/gallery/gallery.php
@@ -1,2 +1,24 @@
 <?php
 global $fields;
+$i = 0;
+?>
+
+<section id="gallery">
+  <div class="slider">
+    <div class="slides">
+      <?php foreach ($fields['gallery'] as $gallery) : ?>
+        <div id="slide-<?= $i; ?>">
+          <img class="img" src="<?= $gallery['url'] ?>" alt="<?= $gallery['title'] ?>">
+          <?php $i++; ?>
+        </div>
+      <?php endforeach; ?>
+    </div>
+    <?php
+    $i = 0;
+    foreach ($fields['gallery'] as $gallery) :
+    ?>
+      <a href="#slide-<?= $i; ?>"></a>
+      <?php $i++; ?>
+    <?php endforeach; ?>
+  </div>
+</section>
