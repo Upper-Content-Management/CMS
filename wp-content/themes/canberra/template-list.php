306c2cadc81e3b6e9a49fec677d3f6e8e3b3525c
Morgan <morgan@wilde.mx>
2020-10-13 16:45:29 +1100

    Created the list page and styled

--- wp-content/themes/canberra/template-list.php
+++ wp-content/themes/canberra/template-list.php
@@ -15,17 +15,32 @@ if ($pagename == 'architects') {
 if ($pagename == 'news') {
   $loop = new WP_Query(array('post_type' => 'article', 'posts_per_page' => 10));
 }
-
-
-while ($loop->have_posts()) : $loop->the_post();
-
-  the_title('<h2 class="entry-title"><a href="' . get_permalink() . '" title="' . the_title_attribute('echo=0') . '" rel="bookmark">', '</a></h2>');
 ?>
+<section id="list">
+  <div class="container">
+    <div class="grid-col-4">
+      <?php
+      while ($loop->have_posts()) : $loop->the_post();
+        $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail'); ?>
+        <div class="frame">
+          <div class="card">
+            <div class="img" style="background-image: url('<?= $image[0]; ?>');"></div>
+            <div class="gradient">
+              <div class="data">
+                <?php
+                the_title('<h3 class="entry-title"><a href="' . get_permalink() . '" title="' . the_title_attribute('echo=0') . '" rel="bookmark">', '</a></h3>');
+                ?>
+              </div>
+            </div>
+          </div>
+          <div class="entry-content">
+            <?php the_content(); ?>
+          </div>
+        </div>
 
-  <div class="entry-content">
-    <?php the_content(); ?>
+      <?php endwhile; ?>
+    </div>
   </div>
-
-<?php endwhile;
-
+</section>
+<?php
 get_footer();
