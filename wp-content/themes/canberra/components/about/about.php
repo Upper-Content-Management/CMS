095e19ff69b27c9dc87cd10906c740ecdaed906e
Morgan <morgan@wilde.mx>
2020-10-16 18:20:45 +1100

    Fixing my mistake

--- wp-content/themes/canberra/components/about/about.php
+++ wp-content/themes/canberra/components/about/about.php
@@ -22,7 +22,7 @@
   <!-- Down arrow navigating to contact section-->
 
   <div class="down-arrow">
-    <a href="#textarea">
+    <a href="#contact">
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
         <path d="M3.375,15.375a12,12,0,1,0,12-12A12,12,0,0,0,3.375,15.375Zm16.679-2.5a1.118,1.118,0,0,1,1.575,0,1.1,1.1,0,0,1,.323.785,1.119,1.119,0,0,1-.329.79l-5.44,5.423a1.112,1.112,0,0,1-1.535-.035l-5.521-5.5A1.114,1.114,0,0,1,10.7,12.756l4.679,4.725Z" transform="translate(-3.375 -3.375)" fill="#DAA520 " /></svg>
     </a>
@@ -30,17 +30,7 @@
       <p>contact us</p>
     </ins>
   </div>
-
-  <div class="form">[contact-form-7 id="354" title="About form"]</div>
-  <div class="details">
-    <h4 class="contact-head2">Our contact information</h4>
-    <ul>
-      <li>Monday - Friday | 9am - 5pm</li>
-      <li>+61 2 6201 5111</li>
-
-      <li>11 Kirinari Street</li>
-
-      <li>Bruce ACT 2617</li>
-    </ul>
+  <div id="contact" class="container-small">
+    <?= $fields['contact'] ?>
   </div>
 </section>
