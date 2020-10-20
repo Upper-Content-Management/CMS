7bfcb21ac3d1716d25e90fe3bcf8f13dba2d47e4
smamone <smamone@users.noreply.github.com>
2020-10-20 19:57:52 +1100

    Actioning suggestions #2
    Categories:
    - Added categories to Houses and Architects pages
    - Styling
    Posts:
    - Previous and next post buttons

--- wp-content/themes/canberra/components/article/textarea/textarea.php
+++ wp-content/themes/canberra/components/article/textarea/textarea.php
@@ -3,11 +3,35 @@ global $fields;
 ?>
 <section id="textarea">
   <div class="container-small">
-      <p class="post_info">
-          Posted by <?php the_author_posts_link(); ?>&nbsp; | &nbsp;
-          <?php the_time('j F Y'); ?>&nbsp; | &nbsp;
-          <?php the_category(', '); ?>
-      </p>
+      <p class="post_author">Posted by <span><?php the_author_posts_link(); ?></span></p>
+      &nbsp; | &nbsp;
+      <p class="post_date"><?php the_time('j F Y'); ?></p>
+      &nbsp; | &nbsp;
+      <p class="cat"><?php the_category(' &nbsp; '); ?></p>
     <div class="text"><?= $fields['text'] ?></div>
+        <?php 
+        $prev_post = get_previous_post();
+        if (!empty( $prev_post )): ?>
+
+            <div class="prev">
+                <a href="<?php echo get_permalink( $prev_post->ID ); ?>">
+                    <p>Previous news item &nbsp;</p>
+                    <?= get_svg('prev'); ?>
+                </a>
+            </div>
+
+        <?php endif; 
+
+        $next_post = get_next_post();
+        if ( is_a( $next_post , 'WP_Post' ) ) { ?>
+
+            <div class="next">
+                <a href="<?php echo get_permalink( $next_post->ID ); ?>">
+                    <p>Next news item  </p>
+                    <?= get_svg('next'); ?>
+                </a>
+            </div>
+
+        <?php } ?>
   </div>
 </section>
